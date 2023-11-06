421ff7ead03a6fd9241d87e230c72bdf38fd595a
Pierre Pronchery <pierre@freebsdfoundation.org>
2023-11-06 18:40:48 +0100

    gbsddialog: fix the build on FreeBSD

--- src/builders.c
+++ src/builders.c
@@ -206,7 +206,7 @@ int builder_infobox(struct bsddialog_conf const * conf,
 	{
 		buttons = GTK_BUTTONS_NONE;
 		id.id = g_timeout_add(conf->sleep * 1000,
-				G_CALLBACK(_infobox_on_timeout), &id);
+				_infobox_on_timeout, &id);
 	}
 	id.dialog = gtk_message_dialog_new(NULL, 0, GTK_MESSAGE_INFO,
 			buttons, "%s", text);
